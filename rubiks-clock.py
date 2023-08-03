d84d4161bd34751f5a5f2f2ebecd65c52df73230
Michael Hirst <mike.n.hirst@gmail.com>
2023-08-03 00:36:16 -0400

    Simple python program to sequence through the videos. Updated file for midnight, which takes longer to finish compared to other minutes.

---
+++ rubiks-clock.py
@@ -0,0 +1,58 @@
+# This python script shows the time of day on the face of a Rubik's cube
+# The script is very simple.  Every minute it plays a short video of a Rubik's cube
+# transitioning to show a new set of numbers on the front.
+#
+# The videos are captured from the rubiks-clock javascript webpage that can be found running here:
+#   rubiksclock.com
+#   24.rubiksclock.com
+#
+# This is intended to run on a Raspberry Pi computer as it relies on the vlc video player.
+# The technique of showing simple videos may be useful for less-powerful computers that may not
+# be able to handle the complex calculations of the javascript from rubiksclock.com . But it has
+# the disadvantage the it uses a lot of sdcard memory to store the images.  The image quality is not
+# nearly as good for the videos compared to the vector graphics from the original javascript webpage.
+#
+# Author: Michael Hirst
+# License: MIT
+#
+
+import schedule
+import time
+import datetime
+import subprocess
+import vlc
+
+
+def showNextMinute():
+
+    global vlcInstance
+    
+    now = datetime.datetime.now()
+    soon = now + datetime.timedelta(0,3) # 0 days,  3 seconds in the future
+
+    hour = int(soon.hour)
+    min = int(soon.minute)
+    filename = f"videos/12hourclock-800x480/rubiks-clock-{hour:02d}{min:02d}.mpg"
+    
+    video = vlcInstance.media_new(filename)
+    player.set_media(video)
+
+    player.play()
+
+
+vlcInstance = vlc.Instance("--aout=alsa")
+player = vlcInstance.media_player_new()
+
+showNextMinute()
+
+schedule.every().minute.at(":59").do(showNextMinute)
+
+try:
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+except KeyboardInterrupt:
+
+    player.stop()
+
